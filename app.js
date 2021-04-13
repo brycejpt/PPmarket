ebd86c90ff796c9578ab6d248896f6341440e1c2
brycejpt <jiapeitong@126.com>
2021-04-13 20:57:01 +0800

    0413

---
+++ app.js
@@ -0,0 +1,61 @@
+// const { Header } = require("antd/lib/layout/layout")
+
+// app.js
+App({
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    console.log()
+    wx.login({
+      success:res =>{
+        console.log(res.code)
+        wx.request({
+          url: 'https://www.pkujpt.cn/getopenid',
+          method:'POST',
+          data:{
+            'code':res.code
+          },
+          header:{
+            'content-type':'application/json'
+          },
+          success(res){
+            console.log(res.data)
+            wx.setStorageSync('uid', res.data.uid)
+          }
+
+
+          // url: 'https://www.pkujpt.cn/getcode',
+          // data: {
+          //   abc:res.code
+          // },
+          // success(res){
+          //   console.log(res.data)
+          // }
+        })
+      }
+    })
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    
+  }
+})
---
+++ pages/mine/mine.js
@@ -0,0 +1,116 @@
+// pages/mine/mine.js
+const app = getApp()
+
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    uid:"000",
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUseGetUserProfile: false,
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') ,
+    nickName: " 123",
+    avatarUrl:" "
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+    console.log(this.data.canIUseGetUserProfile)
+    console.log(this.data.nickName)
+    console.log('123456')
+    this.setData({
+      uid: (wx.getStorageSync('uid')||" ")
+    })
+    // this.setData({
+    //   nickName:this.data.userInfo.nickName
+    // })
+  },
+
+  onShow(){
+    
+  },
+
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res.userInfo.nickName)
+        
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  tomylike: function(){
+    let id = this.data.uid
+    console.log(id)
+    wx.navigateTo({
+      url: '../mylike/mylike?uid='+id,
+    })
+  }
+
+})
